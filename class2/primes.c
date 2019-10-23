4ddf0264e919ee7fbf555cd464df825fe9ef0c1b
Prajwal <gprajwalpoojari@gmail.com>
2019-10-24 01:03:30 +0530

    prime factors

---
+++ class2/primes.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void){
+  int n;
+  printf("Enter a nunber to factorize:\n");
+  scanf("%d",&n);
+  printf("Prime factors:\n");
+  for(int i=1; i<=n; i++){
+    if(i>1){
+      if(n%i==0){
+        while(n%i==0){
+          n=n/i;
+        }
+      printf("%d\n",i);
+      }
+    }
+    else{
+    printf("%d\n",i);
+    }
+  }  
+}
